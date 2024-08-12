61f8a5c6c3346da7940da24d2068b7d692163286
Samuel Colvin <s@muelcolvin.com>
2024-08-12 17:53:34 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::env;
+use std::time::Instant;
+use anyhow::Context;
+use redis::AsyncCommands;
+
+async fn get_redis_value() -> anyhow::Result<()> {
+    let connection_params = env::var("REDIS_DSN").context("REDIS_DSN not set")?;
+    let client = redis::Client::open(connection_params)?;
+    let mut con = client.get_multiplexed_async_connection().await?;
+
+    println!("setting foo...");
+    let start = Instant::now();
+    con.set("foo", "bar").await?;
+    println!("set foo in {:?}", start.elapsed());
+
+    println!("getting foo...");
+    let start = Instant::now();
+    let value: Option<String> = con.get("foo").await?;
+    println!("get foo in {:?} value={:?}", start.elapsed(), value);
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    if let Err(e) = get_redis_value().await {
+        eprintln!("Error: {}", e);
+    }
+}
