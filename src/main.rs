60d68a6d044d16928ed5a9afa1558357e1a49e2f
Samuel Colvin <s@muelcolvin.com>
2024-08-12 18:05:14 +0100

    add object store

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::env;
 use std::time::Instant;
+
 use anyhow::Context;
 use redis::AsyncCommands;
+use object_store::gcp::GoogleCloudStorageBuilder;
+use object_store::{ObjectStore, PutPayload};
+use object_store::path::Path;
 
-async fn get_redis_value() -> anyhow::Result<()> {
-    let connection_params = env::var("REDIS_DSN").context("REDIS_DSN not set")?;
-    let client = redis::Client::open(connection_params)?;
+async fn test_test() -> anyhow::Result<()> {
+    let redis_dsn = env::var("REDIS_DSN").context("REDIS_DSN not set")?;
+    let client = redis::Client::open(redis_dsn)?;
     let mut con = client.get_multiplexed_async_connection().await?;
 
     println!("setting 'foo'...");
@@ -25,9 +29,29 @@ async fn get_redis_value() -> anyhow::Result<()> {
     Ok(())
 }
 
+async fn object_store_test() -> anyhow::Result<()> {
+    let gcs_dsn = env::var("GCS_DSN").context("GCS_DSN not set")?;
+    let store = GoogleCloudStorageBuilder::from_env().with_url(gcs_dsn).build()?;
+    let path = Path::parse(".testing/file.txt")?;
+    let payload = PutPayload::from_static(b"this is a test");
+    let start = Instant::now();
+    store.put(&path, payload).await?;
+    println!("put in {:?}", start.elapsed());
+
+    let start = Instant::now();
+    let payload = store.get(&path).await?;
+    println!("get in {:?}", start.elapsed());
+    println!("returned payload={:?}", payload);
+
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() {
-    if let Err(e) = get_redis_value().await {
+    if let Err(e) = test_test().await {
+        eprintln!("Error: {}", e);
+    }
+    if let Err(e) = object_store_test().await {
         eprintln!("Error: {}", e);
     }
 }
